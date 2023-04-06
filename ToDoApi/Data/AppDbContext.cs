a418069c0b9a332808ea89725023335f0eb7c457
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-04-07 01:19:24 +0300

    Добавлена БД и сервис получения всех заметок

---
+++ ToDoApi/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using ToDoApi.Models;
+
+namespace ToDoApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<ToDo> ToDos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ToDo>().HasData(new ToDo
+            {
+                Id = "1",
+                Name = "Test 1"
+            });
+
+            modelBuilder.Entity<ToDo>().HasData(new ToDo
+            {
+                Id = "2",
+                Name = "Test 2"
+            });
+
+            modelBuilder.Entity<ToDo>().HasData(new ToDo
+            {
+                Id = "3",
+                Name = "Test 3"
+            });
+        }
+
+    }
+}
--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("api/todo", async (AppDbContext context) =>
+{
+    var items = await context.ToDos.ToListAsync();
+
+    return Results.Ok(items);
+});
 
 app.Run();
