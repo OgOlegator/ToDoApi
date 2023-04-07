802469b8a36ebf124199c3e8445fc4b69dd5b9b7
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-04-07 19:55:11 +0300

    Добавлена логика АПИ

---
+++ ToDoApi/Migrations/20230407160526_AddDataInDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ToDoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataInDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { "1", "Test 1" },
+                    { "2", "Test 2" },
+                    { "3", "Test 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ToDos",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "ToDos",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "ToDos",
+                keyColumn: "Id",
+                keyValue: "3");
+        }
+    }
+}
--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
+using ToDoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +10,75 @@ builder.Services.AddDbContext<AppDbContext>(options
 
 var app = builder.Build();
 
+//Get all
 app.MapGet("api/todo", async (AppDbContext context) =>
 {
     var items = await context.ToDos.ToListAsync();
 
     return Results.Ok(items);
+});
+
+//Get by id
+app.MapGet("api/todo/{id}", async (string id, AppDbContext context) =>
+{
+    var item = await context.ToDos.FirstOrDefaultAsync(x => x.Id == id);
+
+    return Results.Ok(item);
+});
+
+//Create todo
+app.MapPost("api/todo", async([FromBody] ToDo newToDo, AppDbContext context) =>
+{
+    try
+    {
+        context.ToDos.Add(newToDo);
+        await context.SaveChangesAsync();
+        return Results.Ok("ToDo created");
+    }
+    catch
+    {
+        return Results.Problem();
+    }
+});
+
+//Edit todo
+app.MapPut("api/todo", async ([FromBody] ToDo changeToDo, AppDbContext context) =>
+{
+    var toDo = await context.ToDos.FirstOrDefaultAsync(x => x.Id == changeToDo.Id);
+
+    if (toDo == null)
+        return Results.NotFound();
+
+    try
+    {
+        context.ToDos.Update(changeToDo);
+        await context.SaveChangesAsync();
+        return Results.Ok("ToDo changed");
+    }
+    catch
+    {
+        return Results.Problem();
+    }
+});
+
+//Delete todo
+app.MapDelete("api/todo/{id}", async (string id, AppDbContext context) =>
+{
+    var toDo = await context.ToDos.FirstOrDefaultAsync(x => x.Id == id);
+
+    if (toDo == null)
+        return Results.NotFound();
+
+    try
+    {
+        context.ToDos.Remove(toDo);
+        await context.SaveChangesAsync();
+        return Results.Ok("ToDo deleted");
+    }
+    catch
+    {
+        return Results.Problem();
+    }
 });
 
 app.Run();
