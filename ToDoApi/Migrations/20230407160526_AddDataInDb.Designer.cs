b09017564a9bf25200fcdb64c1b3022117a7f2b4
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-04-07 19:55:11 +0300

    Добавлена логика АПИ

--- ToDoApi/Migrations/20230407160526_AddDataInDb.Designer.cs
+++ ToDoApi/Migrations/20230407160526_AddDataInDb.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoApi.Data;
 
@@ -10,9 +11,11 @@ using ToDoApi.Data;
 namespace ToDoApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407160526_AddDataInDb")]
+    partial class AddDataInDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
