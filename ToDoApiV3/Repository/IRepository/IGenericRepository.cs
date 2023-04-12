f782613e4baa1823ab2ec2e2505d93b8d74f8d42
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-04-12 23:52:53 +0300

    Добавлены все приготовления для реализации обощенного репозитория

---
+++ ToDoApiV3/Repository/IRepository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoApiV3.Repository.IRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+
+        IEnumerable<TEntity> Get();
+
+        TEntity GetById(string id);
+
+        void Create(TEntity item);
+
+        void Update(TEntity item);
+
+        void Delete(TEntity item);
+        
+    }
+}
